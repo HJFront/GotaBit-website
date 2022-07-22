e12f5c65c3b8d5bc7b820e161c4dabd3557beba5
nanqiang <nanqianglin@gmail.com>
2022-07-22 23:12:03 +0800

    chore: relocate the ErrorBoundary component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 import CssBaseline from '@mui/material/CssBaseline'
 import { CacheProvider, EmotionCache } from '@emotion/react'
-import createEmotionCache from '../src/createEmotionCache'
-import InitTheme from '../src/components/InitTheme'
-import InitI18n from '../src/components/InitI18n'
-import ErrorBoundary from 'components/ErrorBoundary';
+import createEmotionCache from 'src/createEmotionCache'
+import InitTheme from 'src/components/InitTheme'
+import InitI18n from 'src/components/InitI18n'
+import ErrorBoundary from 'src/components/ErrorBoundary'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -22,7 +22,7 @@ export default function MyApp(props: MyAppProps) {
       <InitI18n>
         <CacheProvider value={emotionCache}>
           <Head>
-            <meta name="viewport" content="initial-scale=1, width=device-width" />
+            <meta name='viewport' content='initial-scale=1, width=device-width' />
           </Head>
           <InitTheme>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
