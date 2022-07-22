26bb14b44b908e6e4701558c794a8389074cb2e7
WuChenDi <wuchendi96@gmail.com>
2022-07-22 18:29:47 +0800

    feat: add 404 errorBoundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { CacheProvider, EmotionCache } from '@emotion/react'
 import createEmotionCache from '../src/createEmotionCache'
 import InitTheme from '../src/components/InitTheme'
 import InitI18n from '../src/components/InitI18n'
+import ErrorBoundary from 'components/ErrorBoundary';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -17,17 +18,19 @@ interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
-    <InitI18n>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <InitTheme>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </InitTheme>
-      </CacheProvider>
-    </InitI18n>
+    <ErrorBoundary>
+      <InitI18n>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <InitTheme>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </InitTheme>
+        </CacheProvider>
+      </InitI18n>
+    </ErrorBoundary>
   )
 }
