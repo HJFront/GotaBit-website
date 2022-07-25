6a25c1749a5905d9ab2c8038abcc7e3244470827
WuChenDi <wuchendi96@gmail.com>
2022-07-25 19:43:39 +0800

    feat: Develop Main Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 import { Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-import NavBar from 'src/components/NavBar'
+import Main from 'src/components/Home/Main'
 import CoreModules from 'src/components/Home/CoreModules'
 import BuildIng from 'src/components/Home/BuildIng'
 import Footer from 'src/components/Footer'
@@ -13,11 +13,8 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <NavBar />
+      <Main />
       <Container maxWidth='lg'>
-        <Typography component='h1' color='text.primary'>
-          {t('title')}
-        </Typography>
         <CoreModules />
         <BuildIng />
       </Container>
