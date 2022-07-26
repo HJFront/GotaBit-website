ac7272b38ca429fb0e4523c468d566d2e827b97a
WuChenDi <wuchendi96@gmail.com>
2022-07-26 20:10:05 +0800

    feat: add background image to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,49 @@
 import React from 'react'
+import Image from 'next/image'
 import type { NextPage } from 'next'
-import Container from '@mui/material/Container'
-import { useTranslation } from 'react-i18next'
+import { Box, Container } from '@mui/material'
 import Main from 'src/components/Home/Main'
 import CoreModules from 'src/components/Home/CoreModules'
 import BuildIng from 'src/components/Home/BuildIng'
 import Footer from 'src/components/Footer'
 
 const Home: NextPage = () => {
-  const { t } = useTranslation('common')
-
   return (
-    <>
+    <Box
+      position='relative'
+      component='div'
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          left: 0,
+          bottom: '284px',
+          width: '312px',
+          height: '628px',
+          display: ['none', 'inline-block'],
+        }}
+      >
+        <Image src='/images/GroupLeftBg.png' alt='' width={312} height={628} />
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          right: 0,
+          bottom: '1020px',
+          width: '312px',
+          height: '628px',
+          display: ['none', 'inline-block'],
+        }}
+      >
+        <Image src='/images/GroupRightBg.png' alt='' width={312} height={628} />
+      </Box>
       <Main />
       <Container maxWidth='lg'>
         <CoreModules />
         <BuildIng />
       </Container>
       <Footer />
-    </>
+    </Box>
   )
 }
 
