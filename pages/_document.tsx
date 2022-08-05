f9a56e38dc25323505ad38bb2f1583b287312722
WuChenDi <wuchendi96@gmail.com>
2022-08-05 10:16:51 +0800

    feat: add favicon icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='shortcut icon' href={`${prefix}/favicon.ico`} />
+          <link rel='shortcut icon' href={`${prefix}/favicon.svg`} />
           <link
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
