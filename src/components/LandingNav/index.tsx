55690057817e19b9770db9cc125d4f6c06ae5322
nanqiang <nanqianglin@gmail.com>
2022-08-06 00:05:58 +0800

    chore: remove the drawer for mobile

--- src/components/LandingNav/index.tsx
+++ src/components/LandingNav/index.tsx
@@ -1,14 +1,9 @@
-import { AppBar, Box, Drawer, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Toolbar } from '@mui/material'
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 
 import Link from '../Link'
-import MenuIcon from '../NavBar/MenuIcon'
 import Logo from './Logo'
-
-const drawerWidth = '60%'
-const container = typeof window !== undefined ? () => window.document.body : undefined
 
 const LandingNav = () => {
   const { t } = useTranslation('nav')
@@ -71,15 +66,16 @@ const LandingNav = () => {
               <Logo />
             </Link>
           </Box>
-          <Box sx={{ display: ['none', 'flex'], alignItems: 'center', flex: 1, justifyContent: 'flex-end' }}>
+          <Box sx={{ display: ['flex'], alignItems: 'center', flex: 1, justifyContent: 'flex-end' }}>
             {items.map(item => (
               <Link
                 key={item.key}
                 href={item.path}
                 sx={{
-                  fontSize: '18px',
+                  fontSize: ['14px', '18px'],
                   fontWeight: 600,
-                  ml: '40px',
+                  ml: ['14px', '40px'],
+                  mr: ['4px', 0],
                   color: 'rgba(255, 255, 255, 0.7)',
                 }}
               >
@@ -87,65 +83,8 @@ const LandingNav = () => {
               </Link>
             ))}
           </Box>
-          <IconButton
-            color='primary'
-            aria-label='open drawer'
-            edge='start'
-            onClick={handleDrawerToggle}
-            sx={{ display: ['block', 'none'], mr: '-8px', ml: '10px' }}
-          >
-            <MenuIcon />
-          </IconButton>
         </Toolbar>
       </AppBar>
-      <Box component='nav'>
-        <Drawer
-          container={container}
-          variant='temporary'
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          anchor='right'
-          sx={{
-            display: ['block', 'block', 'none'],
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth,
-            },
-            backgroundColor: ['transparent'],
-          }}
-        >
-          <Box>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                p: '10px',
-              }}
-            >
-              <CloseRoundedIcon color='info' fontSize='medium' onClick={handleDrawerToggle} />
-            </Box>
-            <Box>
-              {items.map(item => (
-                <Typography key={item.key} component='p'>
-                  <Link
-                    href={item.path}
-                    sx={{
-                      fontSize: '18px',
-                      fontWeight: 600,
-                      ml: '20px',
-                    }}
-                  >
-                    {item.label}
-                  </Link>
-                </Typography>
-              ))}
-            </Box>
-          </Box>
-        </Drawer>
-      </Box>
     </Box>
   )
 }
